c53ba312f04b7b7e15cdfd51408040e8cba51f55
ricarthlima <ricarth.lima@gmail.com>
2025-10-28 11:47:43 -0300

    chore: configurado firebase para projeto dev

--- vagarin_app/lib/main.dart
+++ vagarin_app/lib/main.dart
@@ -1,6 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:vagarin_app/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // TODO: No futuro, podemos separar dev, homolog e prod em projetos
+  // Firebase diferentes, mas para essa entrega não é necessário.
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MainApp());
 }
 
@@ -10,11 +18,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+      home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
 }
