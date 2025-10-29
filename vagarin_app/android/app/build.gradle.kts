466cd48771dcf2bcb486d2246e8f89116b92a426
ricarthlima <ricarth.lima@gmail.com>
2025-10-29 15:26:22 -0300

    chore: configuracoes de permissoes para android

--- vagarin_app/android/app/build.gradle.kts
+++ vagarin_app/android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 android {
     namespace = "com.example.vagarin_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
